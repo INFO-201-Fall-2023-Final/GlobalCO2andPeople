9be77ec002598bb46efa45a58f76d2cad216990b
khairullah7 <imkhairullah7@gmail.com>
2023-11-27 19:39:43 -0800

    merged dataset but still need to fix issue

--- climatecleaneddata.R
+++ climatecleaneddata.R
@@ -5,3 +5,8 @@ library(stringr)
 #Reads in datasets 
 temp_df <- read.csv("globaltemp.csv") 
 emission_df <- read.csv("emissions.csv") 
+
+#Joins in dataframes
+temp_df$Year <- gsub("F", "", as.character(temp_df$Year))
+year_fix <- as.numeric(df$Year.x)
+df <- merge(emission_df, temp_df, by = "Country", all = TRUE)
