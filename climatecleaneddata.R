0ef1e2d8a1e424b02fd2912a65dc267bd00ca432
khairullah7 <imkhairullah7@gmail.com>
2023-11-27 20:42:56 -0800

    added new csv file risk index

--- climatecleaneddata.R
+++ climatecleaneddata.R
@@ -3,10 +3,16 @@ library(dplyr)
 library(stringr)
 
 #Reads in datasets 
-temp_df <- read.csv("globaltemp.csv") 
+#temp_df <- read.csv("globaltemp.csv") 
 emission_df <- read.csv("emissions.csv") 
+risk_df <- read.csv("worldrisk.csv")
 
 #Joins in dataframes
-temp_df$Year <- gsub("F", "", as.character(temp_df$Year))
-year_fix <- as.numeric(df$Year.x)
-df <- merge(emission_df, temp_df, by = "Country", all = TRUE)
+#temp_df$Year <- gsub("F", "", as.character(temp_df$Year))
+#year_fix <- as.numeric(df$Year.x)
+df <- merge(emission_df, risk_df, by = "Year", all = TRUE)
+
+str(emission_df)
+str(risk_df)
+#str(temp_df)
+#str(df)
