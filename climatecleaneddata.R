59ffce0a9b97a177d90eec5aaaba584fba7ee666
khairullah7 <imkhairullah7@gmail.com>
2023-11-27 20:57:47 -0800

    took out temp parts

--- climatecleaneddata.R
+++ climatecleaneddata.R
@@ -10,8 +10,17 @@ risk_df <- read.csv("worldrisk.csv")
 #Joins in dataframes
 #temp_df$Year <- gsub("F", "", as.character(temp_df$Year))
 #year_fix <- as.numeric(df$Year.x)
+emission_df[emission_df[["Year"]] >= "2011", ]
 df <- merge(emission_df, risk_df, by = "Year", all = TRUE)
 
+#Create one new categorical variable
+
+
+#Create one new numerical variable
+
+#Create one summarization dataframe
+
+#Check types in datasets
 str(emission_df)
 str(risk_df)
 #str(temp_df)
