14d5cf105a288c06a740ae757b438a6ef81d1d05
khairullah7 <imkhairullah7@gmail.com>
2023-11-27 22:25:40 -0800

    created a categorical varaible column called co2_levels

--- climatecleaneddata.R
+++ climatecleaneddata.R
@@ -7,14 +7,16 @@ emission_df <- read.csv("emissions.csv")
 risk_df <- read.csv("worldrisk.csv")
 
 #Joins in dataframes
-#df[df$Year != 1990, 1991 ,1992 ,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010, ]
 df <- merge(emission_df, risk_df, by = "Year", all = TRUE)
 df <- na.omit(df) 
 print(na.omit(df))
 
 
 #Create one new categorical variable
-
+df$CO2EmissionRate..mt. <- as.numeric(df$CO2EmissionRate..mt.)
+df$co2_levels <- as.factor(ifelse(df$CO2EmissionRate..mt. < 1, "Low emission rate" ,
+                 ifelse(df$CO2EmissionRate..mt. < 10, "Medium emission rate",
+                 ifelse(df$CO2EmissionRate..mt. < 30, "High emission rate", "Extreme emission rate"))))
 
 #Create one new numerical variable
 
