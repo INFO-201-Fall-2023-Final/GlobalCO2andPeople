25a52dc00e8d3934a7ddc418a51acf6a55b2af49
khairullah7 <imkhairullah7@gmail.com>
2023-12-07 14:19:51 -0800

    Updated pages and layout of shiny app

--- app.R
+++ app.R
@@ -2,11 +2,32 @@ library(shiny)
 
 global_df <- read.csv("global_avg_data.csv")
 
-ui <- navbarPage("Global CO2 and Population",
-  tabPanel("Home"),
-  tabPanel("Page 1"),
-  tabPanel("Page 2"),
-  tabPanel("Page 3")
+home_panel <- fluidPage(
+  h1("Welcome to the home page!"),
+  p("Write some information here")
+)
+
+page1_panel <- fluidPage(
+  h1("This is the first page"),
+  p("Write some infomration here")
+)
+
+page2_panel <- fluidPage(
+  h1("This is the second page"),
+  p("Write some infomration here")
+)
+
+page3_panel <- fluidPage(
+  h1("This is the third page"),
+  p("Write some infomration here")
+)
+
+ui <- navbarPage(
+  "Global CO2 and Population",
+  tabPanel("Home", home_panel),
+  tabPanel("Page 1", page1_panel),
+  tabPanel("Page 2", page2_panel),
+  tabPanel("Page 3", page3_panel)
 )
 
 server <- function(input, output){
